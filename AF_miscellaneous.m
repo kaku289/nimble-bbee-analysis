61271de79088dd237cd268b8cd690d445eb5a035
reken001 <pulkit.times@gmail.com>
2020-09-14 09:37:19 +0200

    Code backed-up

--- AF_miscellaneous.m
+++ AF_miscellaneous.m
@@ -75,3 +75,75 @@ for ct_pattern = 2%1:length(pattern)
         end
     end
 end
+
+%% Create data files to be uploaded with manuscript
+clc; close all;
+clear;
+% 
+inputFile = '/media/reken001/Disk_08_backup/light_intensity_experiments/postprocessing/BlindLandingtracks_A1_rref.mat';
+load(inputFile);
+treatments = treatments(1:14*8); % Taking experiments for 2 patterns * 3 lights
+
+outputFile = '/media/reken001/Disk_08_backup/light_intensity_experiments/postprocessing/landingTracks_forUpload.mat';
+
+pattern = {'checkerboard', 'spokes'};
+light = {'low', 'medium', 'high'};
+behaviour = {'rising','constant','sleeping'};
+
+factors = [0.25:0.25:2.5];
+chosen_fac = 1;
+
+clear dummy data;
+data = struct.empty;
+
+for ct_pattern = 1:length(pattern)
+    for ct_light = 1:length(light)
+        dummy = struct.empty;
+        for ct_behaviour = 2%1:length(behaviour)
+            disp(' ');
+            disp(['Pattern: ' pattern{ct_pattern} ...
+                  ', light: ' light{ct_light} ...
+                  ', behaviour: ' behaviour{ct_behaviour}]);
+        
+           % Selecting relevant treatments
+            if strcmpi(behaviour{ct_behaviour}, 'rising')
+                relevantTreatments = treatments(strcmpi({treatments.pattern}, pattern{ct_pattern}) & ...
+                                     strcmpi({treatments.light}, light{ct_light}) & ...
+                                     rem(1:length(treatments), 8)==1);
+            elseif strcmpi(behaviour{ct_behaviour}, 'constant')
+                relevantTreatments = treatments(strcmpi({treatments.pattern}, pattern{ct_pattern}) & ...
+                                     strcmpi({treatments.light}, light{ct_light}) & ...
+                                     rem(1:length(treatments), 8)>1 & ...
+                                     rem(1:length(treatments), 8)<8);
+            elseif strcmpi(behaviour{ct_behaviour}, 'sleeping')
+                relevantTreatments = treatments(strcmpi({treatments.pattern}, pattern{ct_pattern}) & ...
+                                     strcmpi({treatments.light}, light{ct_light}) & ...
+                                     rem(1:length(treatments), 8)==0);
+            else
+                error('What other treatments did you perform dude?')
+            end
+            
+            %%%%%%%%%%%%%% Each light and pattern combination %%%%%%%%%%%
+            for ct_treatment = 1:length(relevantTreatments)
+                landingTracks = [relevantTreatments(ct_treatment).landingTracks];
+                state_LDF = [landingTracks.state_LDF];
+                
+                for ct_track=1:length(state_LDF)
+                    dummy(end+1).state = state_LDF(ct_track).filteredState(:,1:4);
+                    dummy(end).state(:,3) = -dummy(end).state(:,3);
+                    dummy(end).date = relevantTreatments(ct_treatment).datenum;
+                    dummy(end).pattern = relevantTreatments(ct_treatment).pattern;
+                    dummy(end).light = relevantTreatments(ct_treatment).light;
+                    dummy(end).treatmentStartTime = relevantTreatments(ct_treatment).startTime;
+                    dummy(end).treatmentEndTime = relevantTreatments(ct_treatment).endTime;
+                end
+            end
+            
+            disp(size(dummy));
+            
+            data = [data dummy];
+            
+        end
+    end
+end
+save(outputFile, 'data');
